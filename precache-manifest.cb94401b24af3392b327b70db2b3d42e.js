65984ce7fb5cc216dd0194851b9783304c57754a
Lhester Monroyo <lhestermoon3011@gmail.com>
2020-08-31 15:19:05 +0800

    Updates

---
+++ precache-manifest.cb94401b24af3392b327b70db2b3d42e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd25a3b1de8f9f4ca3660c242303fc1",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "fdecbb9060676385f759",
+    "url": "/instagram-clone/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1a7c4351700153d9a1f8",
+    "url": "/instagram-clone/static/js/2.2f39539b.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/instagram-clone/static/js/2.2f39539b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdecbb9060676385f759",
+    "url": "/instagram-clone/static/js/main.4b99bfff.chunk.js"
+  },
+  {
+    "revision": "9d73230a1dae251b7590",
+    "url": "/instagram-clone/static/js/runtime-main.8aef135a.js"
+  },
+  {
+    "revision": "335709b075cd3ee707ead2f069336518",
+    "url": "/instagram-clone/static/media/1.335709b0.png"
+  },
+  {
+    "revision": "027732f527c31fab1965b9eae5e8c5a6",
+    "url": "/instagram-clone/static/media/2.027732f5.png"
+  },
+  {
+    "revision": "e0e9a8d5af5b30418912df5deb86d642",
+    "url": "/instagram-clone/static/media/3.e0e9a8d5.png"
+  },
+  {
+    "revision": "88d8a3f03905f6b865d85413db63bd21",
+    "url": "/instagram-clone/static/media/4.88d8a3f0.png"
+  },
+  {
+    "revision": "43a43720d4746e37b025dafc11978093",
+    "url": "/instagram-clone/static/media/5.43a43720.png"
+  },
+  {
+    "revision": "7a0dd4b5ba2a54bde6b51abe611fcf6c",
+    "url": "/instagram-clone/static/media/avatar.7a0dd4b5.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/instagram-clone/static/media/logo-icon.097b7749.png"
+  },
+  {
+    "revision": "0dec417928863d43f88a01ec70a89eaf",
+    "url": "/instagram-clone/static/media/logo-text.0dec4179.png"
+  }
+]);
